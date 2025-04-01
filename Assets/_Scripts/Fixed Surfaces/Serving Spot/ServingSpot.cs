22e57a239801e01dd36c378496e79e299772126e
Vitor Betmann <vitorbetmann@icloud.com>
2025-04-01 01:52:00 -0400

    Customer's take orders now
    Title

---
+++ Assets/_Scripts/Fixed Surfaces/Serving Spot/ServingSpot.cs
@@ -0,0 +1,40 @@
+using _Scripts.Customer;
+using _Scripts.Food;
+using UnityEngine;
+using static _Scripts.Customer.CustomersManager.LineOfCustomers;
+
+namespace _Scripts.Fixed_Surfaces.Serving_Spot
+{
+    public class ServingSpot : Surface
+    {
+        private CustomersManager customersManager;
+        private BoxCollider2D col;
+
+        private new void Start()
+        {
+            base.Start();
+            col = GetComponent<BoxCollider2D>();
+        }
+
+        private void Update()
+        {
+            if (transform.childCount == 0)
+            {
+                col.enabled = true;
+                return;
+            }
+
+            col.enabled = false;
+            if (!transform.GetChild(0).TryGetComponent<Order>(out var order)) return;
+            foreach (var customer in Line)
+            {
+                if (!customer.TryGetComponent<Customer.Customer>(out var cstmr) ||
+                    !cstmr.Order.name.Equals(order.name)) continue;
+                cstmr.IsServed = true;
+                order.SayByeBye();
+                //RemoveCustomerFromLine(cstmr.gameObject);
+                break;
+            }
+        }
+    }
+}
