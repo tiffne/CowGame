d296451da309a0f949fa3eee2a736b80156afd32
Katsa <bejemilk@my.yorku.ca>
2025-04-10 23:10:11 -0400

    Added cursor changing

---
+++ Assets/_Scripts/DressUpButtonManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] private Texture2D cowSprite;
+    [SerializeField] private  Texture2D predatorSprite;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.SetCursor(cowSprite, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void LionClick()
+    {
+        Debug.Log("Lion Button Clicked");
+        Cursor.SetCursor(predatorSprite, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void WolfClick()
+    {
+        Debug.Log("Wolf Button Clicked");
+        Cursor.SetCursor(predatorSprite, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void CowClick()
+    {
+        Debug.Log("Cow Button Clicked");
+        Cursor.SetCursor(cowSprite, Vector2.zero, CursorMode.Auto);
+    }
+}
